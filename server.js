70526e411ed9961e02d3aa4949b3bfcfa6b6c0c6
Gaurav <gaurav.kumar@exusys.com>
2020-05-28 12:33:23 +0530

    updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Node.js® is a JavaScript runtime built on Chrome');
+  res.send('Node. js is a platform built on Chrome JavaScript runtime for easily building fast and scalable network applications. Node. js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.');
 });
 
 app.listen(PORT, HOST);
