704e6a00fa70f9bf5251664fc866ceda75690026
Gaurav <gaurav.kumar@exusys.com>
2020-05-30 13:49:06 +0530

    updated file 5

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Node. fast and scalable network applications. Node. js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.');
+  res.send('Node. fast and scalable network applications.');
 });
 
 app.listen(PORT, HOST);
